1c410414603c7900585962e67578a19eacbfd889
TrampI550 <58778314+TrampI550@users.noreply.github.com>
2022-11-12 02:42:08 +0300

    lab6 and lab7 dop tasks

--- lab6/app/src/main/java/com/example/lab6/CheckBoxState.java
+++ lab6/app/src/main/java/com/example/lab6/CheckBoxState.java
@@ -3,6 +3,7 @@ package com.example.lab6;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -16,7 +17,10 @@ public class CheckBoxState extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_box_state);
+        EditText login = findViewById(R.id.editTextTextPersonName);
+        EditText password = findViewById(R.id.editTextTextPassword);
         CheckBox checkBox = findViewById(R.id.checkBox);
+        Button login_btn = findViewById(R.id.login_btn);
         final boolean[] checked = {PreferenceManager.getDefaultSharedPreferences(this).getBoolean("checkBox", false)};
         checkBox.setChecked(checked[0]);
         checkBox.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +31,23 @@ public class CheckBoxState extends AppCompatActivity {
                         .edit().putBoolean("checkBox", checked[0]).commit();
             }
         });
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String to_login = String.valueOf(login.getText());
+                String to_pass = String.valueOf(password.getText());
+                PreferenceManager.getDefaultSharedPreferences(CheckBoxState.this)
+                        .edit().putString("login", to_login).commit();
+                PreferenceManager.getDefaultSharedPreferences(CheckBoxState.this)
+                        .edit().putString("password", to_pass).commit();
+            }
+        });
+        final String[] log = {PreferenceManager.getDefaultSharedPreferences(this)
+                .getString("login", "")};
+        final String[] par = {PreferenceManager.getDefaultSharedPreferences(this)
+                .getString("password", "")};
+        login.setText(log[0]);
+        password.setText(par[0]);
         View saveLayout = findViewById(R.id.SaveLayout);
         EditText editText = findViewById(R.id.SaveNote);
         final String[] text = {PreferenceManager.getDefaultSharedPreferences(this)
--- lab7/app/src/main/java/com/example/lab7/ThirdActivity.java
+++ lab7/app/src/main/java/com/example/lab7/ThirdActivity.java
@@ -2,19 +2,24 @@ package com.example.lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
-
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import kotlin.jvm.internal.Ref;
-
 public class ThirdActivity extends AppCompatActivity {
     String time;
+    String time_tosend;
     Timer timer;
     Button start_btn;
     private boolean bolly = true;
@@ -26,8 +31,15 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayList str = new ArrayList<String>();
+        ListAdapter customAdapter = new ListAdapter(this, R.layout.list_items, str, 1);
+        listView.setAdapter(customAdapter);
+
         idTimer = findViewById(R.id.idTimer);
         start_btn = findViewById(R.id.start_btn);
+        Button add_time = findViewById(R.id.add_time);
         start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +53,7 @@ public class ThirdActivity extends AppCompatActivity {
                                     time += (hour < 10) ? "0" + String.valueOf(hour) + ":" : String.valueOf(hour) + ":";
                                     time += (minute < 10) ? "0" + String.valueOf(minute) + ":" : String.valueOf(minute) + ":";
                                     time += (second < 10) ? "0" + String.valueOf(second) : String.valueOf(second);
+                                    time_tosend = time;
                                     second++;
                                     hour = (hour + (minute + second / 60) / 60) % 24;
                                     minute = (minute + second / 60) % 60;
@@ -59,5 +72,45 @@ public class ThirdActivity extends AppCompatActivity {
                 }
             }
         });
+        add_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str.add(time_tosend);
+                customAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+    private static class ListAdapter extends ArrayAdapter {
+
+        private int resourceLayout;
+        private Context mContext;
+        private ArrayList<String> source;
+        private int counter;
+
+        public ListAdapter(Context context, int resource, ArrayList<String> str, int _counter) {
+            super(context, resource, str);
+            this.resourceLayout = resource;
+            this.mContext = context;
+            this.source = str;
+            this.counter = _counter;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View v = convertView;
+
+            if (v == null) {
+                LayoutInflater vi;
+                vi = LayoutInflater.from(mContext);
+                v = vi.inflate(resourceLayout, null);
+            }
+
+            LinearLayout row = v.findViewById(R.id.Rectangle);
+            row.setBackgroundColor(Color.WHITE);
+            TextView item = v.findViewById(R.id.Item);
+            item.setText(Integer.toString(position+1) + " " + source.get(position));
+            return v;
+        }
     }
 }
